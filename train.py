10417632e932e091d03685c8519e8e90a519f055
Deepali Jain <djdeepalijain811@gmail.com>
2016-04-20 16:40:40 +0530

    Save model periodically while training

--- train.py
+++ train.py
@@ -11,14 +11,14 @@ out_neurons = 1
 model = model_utils.create_network(in_neurons, out_neurons)
 
 print "Training ..."
-iterations = 50
-epochs_per_iteration = 10
+iterations = 100
+epochs_per_iteration = 5
 batch_size = 10000
 
 for iteration in range(iterations):
-	print "Iteration Number: " + str(i)
+	print "Iteration Number: " + str(iteration)
 	model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=epochs_per_iteration)  
-	fname = os.path.join('models','trained_model_' + str(i) + '.hdf5')
+	fname = os.path.join('models','trained_model_' + str(iteration) + '.hdf5')
 	model.save_weights(fname)
 
 print "Training complete"
